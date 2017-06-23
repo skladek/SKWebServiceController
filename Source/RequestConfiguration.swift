c7163fe4ec5832f8086bb4566d9c38b2bb5e40fa
Sean Kladek <sean@303software.com>
2017-06-23 16:47:44 -0600

    Improved documentation

--- Source/RequestConfiguration.swift
+++ Source/RequestConfiguration.swift
@@ -16,9 +16,11 @@ public class RequestConfiguration {
     /// Initializes a request configuration object
     ///
     /// - Parameters:
-    ///   - additionalHTTPHeaders: Additional HTTP headers to append to the request.
-    ///        This will override any settings provided by the URLSessionConfiguration.
-    ///   - queryParameters: Query parameters to append to the endpoint.
+    ///   - additionalHTTPHeaders: Additional HTTP headers to append to the request. This will override
+    ///        any settings provided by the URLSessionConfiguration. Supported value types are String, Number,
+    ///        and Boolean. Other types will attempt to be converted, but the result is not guaranteed.
+    ///   - queryParameters: Query parameters to append to the endpoint. Supported value types are String, Number,
+    ///        and Boolean. Other types will attempt to be converted, but the result is not guaranteed.
     public init(additionalHTTPHeaders: [AnyHashable : Any]? = nil, queryParameters: [AnyHashable : Any] = [:]) {
         self.additionalHTTPHeaders = additionalHTTPHeaders
         self.queryParameters = queryParameters
