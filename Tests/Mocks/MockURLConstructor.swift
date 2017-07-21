461826ab4cb9aa8ee723a6339aca58e47eaadd0e
Sean Kladek <sean@303software.com>
2017-07-21 16:45:01 -0600

    Added tests, fixed the mocks

--- Tests/Mocks/MockURLConstructor.swift
+++ Tests/Mocks/MockURLConstructor.swift
@@ -4,6 +4,7 @@ import Foundation
 
 class MockURLConstructor: URLConstructable {
     var urlWithEndpointCalled = false
+    var baseURL: String = ""
     var parameters: [AnyHashable : Any]? = nil
     var shouldReturnError = false
 
